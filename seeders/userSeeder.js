bb437ee3f470222527f437637e92a13c923a4317
Gaston <aldohernandez1985@gmail.com>
2022-09-30 14:53:02 -0300

    se hizo el userSeeder

---
+++ seeders/userSeeder.js
@@ -0,0 +1,18 @@
+const { faker } = require("@faker-js/faker");
+const { User } = require("../models");
+
+faker.locale = "es";
+
+module.exports = async () => {
+    const users = [];
+
+    for (let i = 0; i < 10; i++) {
+        users.push({
+            firstname: faker.name.firstName(),
+            lastname: faker.name.lastName(),
+        });
+    };
+
+    await User.bulkCreate(users);
+    console.log("[Database] Se corrió el seeder de Users.");
+};
